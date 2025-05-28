80e4b2da192cc411c58789062150f5c62cecfdf6
Lugh <lhflyextra300@gmail.com>
2025-05-28 18:47:03 +0200

    feat(password): Introduce a password generator.

---
+++ pkg/password/password.go
@@ -0,0 +1,84 @@
+// Package password provides a library for generating random password strings.
+//
+//	generator := password.NewPassword()
+//	generator.AllowRepeat = true
+//	pass, err := generator.Generate(16)
+//	if err != nil {
+//	   log.Fatal(err)
+//	}
+//	log.Printf(pass)
+//
+// Author: Lugh
+package password
+
+import (
+	"crypto/rand"
+	"math/big"
+	"slices"
+)
+
+const (
+	// LowerLetters is the list of lowercase letters.
+	lowerLettersDefault = "abcdefghijklmnopqrstuvwxyz"
+
+	// UpperLetters is the list of uppercase letters.
+	upperLettersDefault = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+	// Digits is the list of permitted digits.
+	digitsDefault = "0123456789"
+
+	// Symbols is the list of symbols.
+	symbolsDefault = "!@#$%^&*()_+"
+
+	// Don't allow repeat by default
+	allowRepeatDefault = false
+)
+
+type password struct {
+	LowerLetters string
+	UpperLetters string
+	Digits       string
+	Symbols      string
+	AllowRepeat  bool
+}
+
+// Creates a new password generator with default configuration.
+// To specified a configuration modify directly the exported field.
+//
+// Default values:
+//   - "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+"
+//   - Don't allow repeat by default.
+func NewPassword() *password {
+	return &password{
+		LowerLetters: lowerLettersDefault,
+		UpperLetters: upperLettersDefault,
+		Digits:       digitsDefault,
+		Symbols:      symbolsDefault,
+		AllowRepeat:  allowRepeatDefault,
+	}
+}
+
+// Generates a password with the given requirements. length is the
+// total number of characters in the password.
+func (p *password) Generate(length int) (string, error) {
+
+	var availableChars string = p.UpperLetters + p.LowerLetters + p.Digits + p.Symbols
+
+	chars := []rune(availableChars)
+
+	passwd := make([]rune, length)
+
+	for i := 0; i < length; i++ {
+	NoRepeat:
+		index, err := rand.Int(rand.Reader, big.NewInt(int64(len(chars))))
+		if err != nil {
+			return "", err
+		}
+		if !p.AllowRepeat && slices.Contains(passwd, chars[index.Int64()]) {
+			goto NoRepeat
+		}
+		passwd[i] = chars[index.Int64()]
+	}
+
+	return string(passwd), nil
+}
